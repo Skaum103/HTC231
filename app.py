71a56a61ad68f03524e82a22114ef31bd01b8698
skaum <skaum103@outlook.com>
2023-11-11 19:19:18 -0400

    init

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import (
+    Flask,
+    request,
+    session,
+    send_file,
+)
+from werkzeug.utils import secure_filename  # Import secure_filename
+from identify import identify_gpt
+
+app = Flask(__name__, template_folder="templates", static_url_path="/static")
+app.secret_key = "super secret key"
+
+
+def check(username, password):
+    if username == "lionel":
+        return password == "antonela240687", 1
+    if username == "cristiano":
+        return password == "mariamadrid1985jr", 2
+    if username == "neymar":
+        return password == "#Q5%7Mj5N&xDP", 3
+    if username == "karim":
+        return password == "xiTSM3nTr6j^m", 4
+    if username == "root":
+        return password == "GTfCR38n3A", 5
+
+    return False, 0
+
+def identify(image):
+    # Generate a secure filename
+    filename = secure_filename(image.filename)
+    # Save the image with a unique filename
+    image.save(f"images/{filename}")
+    path = "images/"+filename
+    identify_gpt(path)
+    result = "none"
+
+    return result
+
+@app.route("/robots.txt")
+def robots():
+    return send_file("static/robots.txt")
+
+@app.route("/identify", methods=["POST"])
+def login():
+    if "userID" in session:
+        return identify(request.files['image'])
+
+    status = 200
+    result = None
+
+    if request.method == "POST":
+        try:
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            result, user_id = check(username, password)
+
+            print(result)
+            result = "success" if result else "fail"
+
+            status = 200 if result == "success" else 403
+
+            if result == "success":
+                session["userName"] = username
+                session["userID"] = user_id
+                return identify(request.files['image'])
+
+        except Exception as e:
+            print(f"Error: {e}")
+            result = "fail"
+            status = 400
+
+    return result, status
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=3000)
+
---
+++ identify.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+import base64
+import requests
+
+def identify_gpt(path):
+    # OpenAI API Key
+    api_key="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"
+    # Function to encode the image
+    def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+
+    # Path to your image
+    image_path = path
+
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": "What’s in this image?"
+              },
+              {
+                "type": "image_url",
+                "image_url": {
+                  "url": f"data:image/jpeg;base64,{base64_image}"
+                }
+              }
+            ]
+          }
+        ],
+        "max_tokens": 300
+    }
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    print(response.json())
+
+if __name__ == "__main__":
+    identify_gpt("images/test.png")
